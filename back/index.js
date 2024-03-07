1f6010a9b914118fc6fb6d9e204947613d2f7e2e
joseafch <cherinerxd12376@gmail.com>
2024-03-07 23:16:51 +0100

    vercel option

--- back/index.js
+++ back/index.js
@@ -2,15 +2,14 @@
 import { Express } from 'express'
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const cors = require('cors')
+
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 const app = express();
 
 server.use(middlewares)
 server.use(router)
-server.use(cors())
-server.use(app)
+
 
 server.listen(3000 || process.env.PORT, () => {
   console.log('JSON Server is running')
